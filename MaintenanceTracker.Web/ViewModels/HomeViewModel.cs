90648b6ad693b8c25f55387e1465d5b1bdad1d6b
veom <jordan@ninjatertle.com>
2014-12-15 15:54:10 +1100

    Preparing the login page.

---
+++ MaintenanceTracker.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MaintenanceTracker.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public LoginViewModel Login { get; set; }
+        public RegisterViewModel Register { get; set; }
+    }
+}
